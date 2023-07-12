3c25151bb73065f1724ce1b802009e732d051ff0
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-07-12 20:19:12 +0200

    add 1.7: unicafe step2 solution add 1.8: unicafe step3 solution

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,12 +8,15 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>
 }
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   return (
     <>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </>
   )
 }
@@ -22,10 +25,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(updatedGood + neutral + bad)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(good + updatedNeutral + bad)
+  }
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(good + neutral + updatedBad)
+  }
+
+  const countAverage = () => {
+    // if NaN, return 0
+    return (good - bad) / all || 0
+  }
+
+  const countPositivePercentage = () => {
+    // if NaN, return 0
+    return (good / all) * 100 || 0
+  }
 
   return (
     <>
@@ -34,7 +61,14 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
       <Header text="statistics" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={countAverage()}
+        positive={countPositivePercentage()}
+      />
     </>
   )
 }
