d8e19f72a1a0a075bedb198f790995a93c59e4c2
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-07-12 20:42:52 +0200

    add 1.11*: unicafe step6 solution

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,9 +10,13 @@ const Button = ({ handleClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text} {value} {text === 'positive' ? '%' : ''}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>
+        {value}
+        {text === 'positive' ? ' %' : ''}
+      </td>
+    </tr>
   )
 }
 
@@ -21,14 +25,16 @@ const Statistics = props => {
     return <p>No feedback given</p>
   }
   return (
-    <>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.all} />
-      <StatisticLine text="average" value={props.average} />
-      <StatisticLine text="positive" value={props.positive} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive} />
+      </tbody>
+    </table>
   )
 }
 
