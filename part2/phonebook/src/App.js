3032711090c7d74c057e69bf7b7831497370dce0
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-07-16 21:13:44 +0200

    add 2.11: The Phonebook Step6 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Filter } from './components/Filter'
 import { PersonForm } from './components/PersonForm'
 import { Persons } from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phoneNumber: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phoneNumber: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122', id: 4 },
-    { name: 'John Doe', phoneNumber: '99-876-543210', id: 5 },
-    { name: 'Jane Smith', phoneNumber: '55-123-456789', id: 6 },
-    { name: 'Alex Johnson', phoneNumber: '77-987-654321', id: 7 },
-    { name: 'Emily Brown', phoneNumber: '88-234-567890', id: 8 },
-    { name: 'Michael Wilson', phoneNumber: '66-345-678901', id: 9 },
-    { name: 'Sarah Davis', phoneNumber: '44-567-890123', id: 10 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
   const [lookingFor, setLookingFor] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const handleNameInputChange = event => {
     setNewName(event.target.value)
