998d365dd9c85cc556ad0fa7ee48525f4ed6a475
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-07-18 16:51:08 +0200

    add 2.20*: Data for countries, step3 solution

--- part2/data-for-countries/src/components/Country.js
+++ part2/data-for-countries/src/components/Country.js
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const Country = ({ country, show }) => {
   const [showCountry, setShowCountry] = useState(show)
+  const [weather, setWeather] = useState(null)
   const name = country.name.common
   const capital = country.capital[0]
   const area = country.area
   const languages = Object.values(country.languages)
   const flag = country.flags.png
+
+  useEffect(() => {
+    if (showCountry) {
+      const api_key = process.env.REACT_APP_OPEN_WEATHER_API_KEY
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${api_key}&units=metric`
+      axios
+        .get(url)
+        .then(response => {
+          setWeather(response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }, [showCountry, name])
 
   const handleShowClick = () => {
     setShowCountry(!showCountry)
@@ -26,6 +43,19 @@ export const Country = ({ country, show }) => {
           })}
         </ul>
         <img src={flag} alt={name + ' flag'} width="100" height="100" />
+        <h2>Weather in {name}</h2>
+        {weather ? (
+          <>
+            <p>temperature {weather.main.temp} Celsius</p>
+            <img
+              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+              alt={name + ' weather icon'}
+            />
+            <p>wind {weather.wind.speed} m/s</p>
+          </>
+        ) : (
+          <p>{'Waiting for weather...'}</p>
+        )}
         <hr />
       </>
     )
