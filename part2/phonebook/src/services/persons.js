48b132f2531399a0b0f316589dc89a4006988c53
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-07-23 14:07:30 +0200

    add 3.11: phonebook full stack solution

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -21,9 +21,11 @@ const deletePerson = id => {
   return request.then(response => response.data)
 }
 
-export default {
+const personsService = {
   getAll,
   create,
   update,
   deletePerson,
 }
+
+export default personsService
