27fcb93caad90526183f4b50a0894def0ca2a801
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-07-30 19:37:03 +0200

    add 4.4: helper functions and unit tests, step2 solution

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -6,3 +6,39 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'My First Blog',
+      author: 'Pudding',
+      url: 'http://www.uddi.com',
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'My Second Blog',
+      author: 'AddictedToPasta',
+      url: 'http://www.waitwhat.com',
+      likes: 23,
+      __v: 0,
+    },
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes([blogs[0]])
+    expect(result).toBe(12)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(35)
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,4 +6,16 @@ const dummy = blogs => {
   }
 }
 
-module.exports = { dummy }
+const totalLikes = blogs => {
+  if (blogs.length === 0) {
+    return 0
+  } else if (blogs.length === 1) {
+    return blogs[0].likes
+  } else {
+    return blogs.reduce((accumulator, currentBlog) => {
+      return accumulator + currentBlog.likes
+    }, 0)
+  }
+}
+
+module.exports = { dummy, totalLikes }
