be16de2fb1ab8a5b656055bbfd842357bdf883f2
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-07-30 19:51:36 +0200

    add 4.5*: helper functions and unit tests, step3 solution

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -42,3 +42,47 @@ describe('total likes', () => {
     expect(result).toBe(35)
   })
 })
+
+describe('favorite blog', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'My First Blog',
+      author: 'Pudding',
+      url: 'http://www.uddi.com',
+      likes: 333,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'My Second Blog',
+      author: 'AddictedToPasta',
+      url: 'http://www.waitwhat.com',
+      likes: 23,
+      __v: 0,
+    },
+  ]
+
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual(null)
+  })
+
+  test('when list has only one blog returned that blog', () => {
+    const result = listHelper.favoriteBlog([blogs[0]])
+    expect(result).toEqual({
+      title: 'My First Blog',
+      author: 'Pudding',
+      likes: 333,
+    })
+  })
+
+  test('of a list is returning the most popular one', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({
+      title: 'My First Blog',
+      author: 'Pudding',
+      likes: 333,
+    })
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -18,4 +18,31 @@ const totalLikes = blogs => {
   }
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) {
+    return null
+  } else if (blogs.length === 1) {
+    return {
+      title: blogs[0].title,
+      author: blogs[0].author,
+      likes: blogs[0].likes,
+    }
+  } else {
+    let iMax = 0
+    let vMax = 0
+    for (let i = 0; i < blogs.length; i++) {
+      if (blogs[i].likes > vMax) {
+        iMax = i
+        vMax = blogs[i].likes
+      }
+    }
+
+    return {
+      title: blogs[iMax].title,
+      author: blogs[iMax].author,
+      likes: vMax,
+    }
+  }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
