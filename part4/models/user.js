5dcad959f2678cbf7d11a8f6d0609d8c279167e6
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-08-03 21:15:29 +0200

    add 4.17: bloglist expansion, step5 solution

--- part4/models/user.js
+++ part4/models/user.js
@@ -18,6 +18,8 @@ const userSchema = new mongoose.Schema({
   ],
 })
 
+userSchema.plugin(uniqueValidator)
+
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -26,8 +28,6 @@ userSchema.set('toJSON', {
     delete returnedObject.passwordHash
   },
 })
-
-userSchema.plugin(uniqueValidator)
 
 const User = mongoose.model('User', userSchema)
 
