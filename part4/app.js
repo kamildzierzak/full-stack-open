c778083328695e825be513d298899fd9d13c79d5
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-08-04 20:58:12 +0200

    add 4.22*: bloglist expansion, step10 solution

--- part4/app.js
+++ part4/app.js
@@ -25,9 +25,10 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
+// app.use(middleware.userExtractor)
 
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
-app.use('/api/blogs', blogsRouter)
 app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,7 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('..//models/user')
-const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,11 +9,6 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
 
   if (body.title === undefined) {
     return response.status(400).json({ error: 'title is missing' })
@@ -28,14 +22,14 @@ blogsRouter.post('/', async (request, response) => {
     body.likes = 0
   }
 
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user._id,
+    user: user.id,
   })
 
   const savedBlog = await blog.save()
@@ -46,16 +40,17 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
   const blog = await Blog.findById(request.params.id)
+  const user = request.user
 
-  if (blog.user.toString() === decodedToken.id.toString()) {
-    await Blog.findByIdAndDelete(request.params.id)
+  if (blog.user.toString() === user.id.toString()) {
+    await Blog.findByIdAndDelete(blog.id)
+    // remove references from blogs array in user
+    await user.updateOne({
+      $pull: {
+        blogs: blog.id,
+      },
+    })
     return response.status(204).end()
   } else {
     return response.status(401).json({ error: 'user invalid' })
