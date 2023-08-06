76cddbb49ae82d18c95ba714a294b1532383b484
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-08-06 14:51:24 +0200

    add 5.7: Blog list frontend, step7 solution

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={() => setVisible(!visible)}>view</button>
+      </div>
+      {visible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes} <button>like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
