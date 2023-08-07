cc0d1d8659873aed4ef2181c8a074f20f923cf98
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-08-07 23:53:32 +0200

    add 5.14: Blog list tests, step2 solution

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,24 +1,47 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('blog renders blog title and author, but does not render url and number of likes by default', async () => {
-  const blog = {
-    user: 1,
-    likes: 15,
-    author: 'John',
-    title: 'I like cheese!',
-    url: 'www.cheeselikers.com',
-  }
+describe('<Blog/>', () => {
+  beforeEach(() => {
+    const user = {
+      username: 'test',
+      name: 'test',
+      password: 'test',
+    }
 
-  render(<Blog blog={blog} />)
+    const blog = {
+      user: user,
+      likes: 15,
+      author: 'John',
+      title: 'I like cheese!',
+      url: 'www.cheeselikers.com',
+    }
 
-  const titleAndAuthor = await screen.findByText('I like cheese! John')
-  const url = screen.queryByText('www.cheeselikers.com')
-  const likes = screen.queryByText(15)
+    render(<Blog blog={blog} user={user} />)
+  })
 
-  expect(titleAndAuthor).toBeDefined()
-  expect(url).toBeNull()
-  expect(likes).toBeNull()
+  test('renders the title and author of the blog, but does not render url and number of likes by default', async () => {
+    const titleAndAuthor = await screen.getByText('I like cheese! John')
+    const url = screen.queryByText('www.cheeselikers.com')
+    const likes = screen.queryByText('likes 15')
+
+    expect(titleAndAuthor).toBeDefined()
+    expect(url).toBeNull()
+    expect(likes).toBeNull()
+  })
+
+  test('renders url and number of likes after button for showing more details is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const url = screen.getByText('www.cheeselikers.com')
+    const likes = screen.getByText('likes 15')
+
+    expect(url).toBeDefined()
+    expect(likes).toBeDefined()
+  })
 })
