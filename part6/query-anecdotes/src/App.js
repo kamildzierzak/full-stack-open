4e31ce9ebc26b7cf3600455806e14c64f452299e
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-08-13 13:00:05 +0200

    add Exercise 6.20 solution

---
+++ part6/query-anecdotes/src/App.js
@@ -0,0 +1,49 @@
+import { useQuery } from 'react-query'
+import { getAnecdotes } from './requests'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+  const handleVote = anecdote => {
+    console.log('vote')
+  }
+
+  const {
+    isLoading,
+    isError,
+    data: anecdotes,
+    error,
+  } = useQuery('anecdotes', getAnecdotes, {
+    retry: 1,
+  })
+
+  if (isLoading) {
+    return <div>loading...</div>
+  }
+
+  if (isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
