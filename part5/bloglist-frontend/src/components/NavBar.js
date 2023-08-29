3c3c3774370ce014182a13c378c74673b9dc913b
Kamil Dzierzak <kamdzierzak@gmail.com>
2023-08-29 22:52:00 +0200

    add 7.20: Styles, step1 solution add 7.21: Styles, step2 part-solution

--- part5/bloglist-frontend/src/components/NavBar.js
+++ part5/bloglist-frontend/src/components/NavBar.js
@@ -1,31 +1,52 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
 
 const NavBar = ({ user, logoutHandler }) => {
   const dispatch = useDispatch()
 
-  const navStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    gap: 5,
-    backgroundColor: '#AAA',
-  }
-
   return (
-    <div style={navStyle}>
-      <Link to="/">blogs</Link>
-      <Link to="/users">users</Link>
-      <p>
-        {user.name} logged in{' '}
-        <button
-          id="logout-button"
-          onClick={() => dispatch(logoutHandler())}
-          type="submit"
-        >
-          Logout
-        </button>
-      </p>
-    </div>
+    <Navbar
+      bg="dark"
+      data-bs-theme="dark"
+      expand="lg"
+      className="bg-body-tertiary"
+    >
+      <Container>
+        <Navbar.Brand href="/">__bloGG</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/">
+                Blogs
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/users">
+                Users
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Nav className="me-1">
+            <Navbar.Text className="me-3">
+              <b>{user.name}</b> logged in
+            </Navbar.Text>
+            <Button
+              variant="outline-primary"
+              id="logout-button"
+              onClick={() => dispatch(logoutHandler())}
+              type="submit"
+            >
+              Logout
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
